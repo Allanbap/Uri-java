f49a1bc1f7e76126d6c972f035e0c4ca838395e8
Allan baptista santos <allanbap@outlook.com.br>
2023-02-22 00:52:43 -0300

    Commit: exercise 1003.

---
+++ Uri/src/EX1003.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+/**
+ * Exercise: https://www.beecrowd.com.br/judge/en/problems/view/1003
+ * Author : Allan Baptista Santos
+ * OBS: public class EX1003 has to be renamed to Main to be accepted.
+ */
+public class EX1003 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println("SOMA = "+ (A+B));
+
+    }
+
+
+}
