108d3ea550f3c1abfc05af06f0ebf26698806e57
Allan baptista santos <allanbap@outlook.com.br>
2023-02-22 00:55:35 -0300

    Commit: exercise 1004.

---
+++ Uri/src/EX1004.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+/**
+ * Exercise: https://www.beecrowd.com.br/judge/en/problems/view/1004
+ * Author : Allan Baptista Santos
+ * OBS: public class EX1004 has to be renamed to Main to be accepted.
+ */
+public class EX1004 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println("PROD = "+ (A*B));
+
+    }
+
+
+}
