e3d8283a94c7cbbd4a63296fa2038eb33c4b0499
Allan baptista santos <allanbap@outlook.com.br>
2023-02-22 00:29:52 -0300

    Commit: exercise 1001.

---
+++ Uri/src/EX1001.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/**
+ * Exercise: https://www.beecrowd.com.br/judge/en/problems/view/1001
+ * Author : Allan Baptista Santos
+ * OBS: public class EX1001 has to be renamed to Main to be accepted.
+ */
+public class EX1001 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println("X = "+ (A+B));
+
+    }
+
+
+}
