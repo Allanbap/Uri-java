049903d01e255e7e0d70db2bd01e624ac225cfff
Allan baptista santos <allanbap@outlook.com.br>
2023-02-25 12:32:38 -0300

    Commit: exercise 1016.

---
+++ Uri/src/EX1016.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+/**
+ * Exercise: https://www.beecrowd.com.br/judge/en/problems/view/1016
+ * Author : Allan Baptista Santos
+ * OBS: public class EX1016 has to be renamed to Main to be accepted.
+ */
+public class EX1016 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        System.out.println((A*2)+" minutos");
+
+    }
+
+
+}
